a45421f9c46d99b6fbd34db3708e005efc3f9b12
AFEScalante <a.fer.esca@gmail.com>
2022-08-23 23:05:22 -0500

    Add mvp version of the timer

---
+++ app.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(shinyWidgets)
+library(lubridate)
+library(glue)
+source("countdownCalculate.R")
+source("spinner.R")
+
+ui <- fluidPage(
+    includeCSS("www/styles.css"),
+    # Flex box container with three items: Date input, animation & output.
+    div(class = "flex-container-main",
+      h1("Countdown Timer"),
+      div(id = "dateInput",
+        airDatepickerInput("date", label = "Choose a date", clearButton = TRUE)
+      ),
+      div(class = "output-days", textOutput(outputId = "outputDays"))
+    )
+  )
+
+server <- function(input, output) {
+  timeLeft <- reactive({
+    invalidateLater(1000)
+    time_remaining(input$date, now())
+  })
+
+  output$outputDays <- renderText({
+    glue::glue("{timeLeft()$days} Days : {timeLeft()$hours} Hours : {timeLeft()$minutes} Minutes : {timeLeft()$seconds} Seconds")
+  })
+}
+
+shiny::shinyApp(ui = ui, server = server)
---
+++ countdownCalculate.R
@@ -0,0 +1,21 @@
+# Script for remaining time calculation
+time_remaining <- function(datepicker, datenow) {
+    time_left <- as.numeric(difftime(datepicker, datenow))
+    days <- floor(time_left)
+    # Extract the hours, minutes, seconds...
+    hours_left <- (time_left %% days) * 24
+    hours <- floor(hours_left)
+    minutes_left <- (hours_left %% hours) * 60
+    minutes <- floor(minutes_left)
+    seconds <- floor((minutes_left %% minutes) * 60)
+
+    list(
+        time_left = time_left,
+        time_now = datenow,
+        datepicker = datepicker,
+        days = days,
+        hours = hours,
+        minutes = minutes,
+        seconds = seconds
+    )
+}
